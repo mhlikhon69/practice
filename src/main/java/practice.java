eee26dd2841890dbba4accf009b61e9c0c290485
mhlikhon69 <mhlikhon2003@gmail.com>
2024-10-23 10:34:41 +0600

    change

--- src/main/java/practice.java
+++ src/main/java/practice.java
@@ -4,6 +4,9 @@ public class practice {
    
     public static void main(String[] args) {
         System.out.println("Good night");
+        System.out.println("Good Morning");
+        System.out.println("Good night");
+        System.out.println("Good night");
     }
     
 }
